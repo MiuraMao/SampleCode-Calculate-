411e4352da8d20471937b7ce3a070eaf5b51f2a9
MiuraMao <m_miura06@outlook.com>
2022-04-12 16:57:47 +0900

    Signed Zero

---
+++ 1.6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+//Signed Zero（符号付きゼロ）
+
+int main(void)
+{
+    double a, b, c, d;
+    a=1.0/0.0; b=-1.0/0.0;  //infの作成
+    printf("a=%f b=%f \n", a, b);
+    c=1.0/a; d=1.0/b;       //符号付きゼロの生成
+    printf("c=%f d=%f \n", c, d);
+    printf("ans1=%f, ans2=%f\n", 1.0/c, 1.0/d);
+
+    return 0;
+}
